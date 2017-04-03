955e9faafd668f47d70793144a7d8153400e1929
root <root@vps391249.ovh.net>
2017-04-03 18:38:25 +0200

    Fixed nodejs container on docker and to install Minimal React Webpack Babel on Docker container

---
+++ nodejs/README.md
@@ -0,0 +1,9 @@
+# minimal-react-webpack-babel-setup
+
+* `git clone git@github.com:rwieruch/minimal-react-webpack-babel-setup.git`
+* cd minimal-react-webpack-babel-setup
+* npm install
+* npm start
+* visit `http://localhost:8080/`
+
+Read: [The Minimal React Webpack Babel Setup](https://www.robinwieruch.de/minimal-react-webpack-babel-setup/)
---
+++ nodejs/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3001',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot-loader!babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
